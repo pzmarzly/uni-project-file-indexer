c7708d795bd337d9774f933741f706741a99c715
pzmarzly <pawo2500@gmail.com>
2022-01-15 20:08:01 +0100

    add action on activation

--- indexme/gui/gui.py
+++ indexme/gui/gui.py
@@ -1,3 +1,5 @@
+import subprocess
+from typing import Any, Callable
 import gi
 import pathlib
 import typer
@@ -27,16 +29,35 @@ class MainWindow:
         self.window.connect("destroy", Gtk.main_quit)
 
         self.store = self.builder.get_object("store")
-        self.search = self.builder.get_object("search")
-        self.search.connect("search-changed", lambda _this: self.update_search())
+        self.treeview = self.builder.get_object("treeview")
+        self.treeview.connect(
+            "row-activated", lambda _this, _num, _col: self._row_activated()
+        )
 
-    def update_search(self):
+        self.search = self.builder.get_object("search")
+        self.search.connect("search-changed", lambda _this: self._update_search())
+
+    def show(self) -> None:
+        self.window.show()
+
+    def set_action(self, action: Callable[[str], Any]) -> None:
+        self.action = action
+
+    def _update_search(self) -> None:
         text = self.search.get_text()
 
         self.store.clear()
         with self.Session() as s:
             for file in GetAllFiles(s, "/").with_name(text).limit(100):
                 self.store.append(None, [file.path, file.name, str(file.size)])
+
+    def _row_activated(self) -> None:
+        store, iter = self.treeview.get_selection().get_selected()
+        if iter is None:
+            return
+        path = store[iter][0]
+        if self.action is not None:
+            self.action(path)
 
 
 @app.command()
@@ -45,5 +66,12 @@ def gui(
         True, help="Open file on selection instead of printing filename"
     )
 ) -> None:
-    MainWindow().window.show()
+    window = MainWindow()
+
+    if open:
+        window.set_action(lambda path: subprocess.call(["xdg-open", path]))
+    else:
+        window.set_action(lambda path: print(path))
+
+    window.show()
     Gtk.main()
